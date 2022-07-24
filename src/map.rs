61310aebb66eb0dd71470b6cbbb4b42c61837576
Aaron Bedra <aaron@aaronbedra.com>
2022-07-24 15:28:39 -0500

    wip

--- src/map.rs
+++ src/map.rs
@@ -88,6 +88,10 @@ impl BaseMap for Map {
             self.index_to_point2d(idx2)
         )
     }
+
+    fn is_opaque(&self, idx: usize) -> bool {
+        self.tiles[idx as usize] != TileType::Floor
+    }
 }
 
 pub fn map_idx(x: i32, y: i32) -> usize {
