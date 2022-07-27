a84b4c214954ce848aa73eefe575f42fc2a1ddf9
Aaron Bedra <aaron@aaronbedra.com>
2022-07-26 21:27:11 -0500

    wip

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -12,10 +12,17 @@ use drunkard::DrunkardsWalkArchitect;
 mod prefab;
 use prefab::apply_prefab;
 
+mod themes;
+pub use themes::*;
+
 const NUM_ROOMS: usize = 20;
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
+
+pub trait MapTheme: Sync + Send {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType;
 }
 
 pub struct MapBuilder {
@@ -24,6 +31,7 @@ pub struct MapBuilder {
     pub monster_spawns: Vec<Point>,
     pub player_start: Point,
     pub amulet_start: Point,
+    pub theme: Box<dyn MapTheme>
 }
 
 impl MapBuilder {
@@ -36,6 +44,12 @@ impl MapBuilder {
 
         let mut mb = architect.new(rng);
         apply_prefab(&mut mb, rng);
+
+        mb.theme = match rng.range(0, 2) {
+            0 => DungeonTheme::new(),
+            _ => ForestTheme::new()
+        };
+
         mb
     }
 
